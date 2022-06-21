da429f6d7d7fdebc40c49431c3fc7b228e68aa26
ymachidagit <y.machida.1013@outlook.jp>
2022-06-21 15:33:10 +0900

    fixed kadai10

--- kadai10_2.py
+++ kadai10_2.py
@@ -25,54 +25,44 @@ weather_data = weather_df[(datetime.datetime(year,month,1) <= weather_df['年月
 weather_target = weather_df[(datetime.datetime(year,month,1) <= weather_df['年月日']) 
                             & (weather_df['年月日'] < datetime.datetime(year,month+1,1))][target].values
 
-excel_path='kadai10_table.xlsx'
-book=openpyxl.load_workbook(filename=excel_path)
-sheet=book['Sheet1']
+book=openpyxl.load_workbook('kadai10_table.xlsx')
+sheet1=book['Sheet1']
+sheet2=book['Sheet2']
 
-def feature_importances_calculator(n_est,max_d):
-    # 説明変数の重要度をグラフに出力
-    rf = RandomForestClassifier(n_estimators=n_est, max_depth=max_d, random_state=1)
-    rf = rf.fit(weather_data, weather_target)
-    importances = rf.feature_importances_
-    plt.figure(figsize=(12, 5))
-    plt.barh(range(len(data)), rf.feature_importances_ , align='center')
-    plt.yticks(np.arange(len(data)), data)
-    plt.show()
-
-def decision_tree_f1(max_d):
-    clf = DecisionTreeClassifier(criterion='entropy', max_depth=max_d)
-    clf = clf.fit(weather_data, weather_target)
-    print('max_depth='+str(max_d))
-    print('f1_score:', f1_score(weather_target, clf.predict(weather_data), average="micro"))
+def decision_tree_f1(end_depth):
+    for max_d in range(1,end_depth+1):
+        sheet1.cell(row=max_d+1, column=2).value=max_d
+        clf = DecisionTreeClassifier(criterion='entropy', max_depth=max_d)
+        clf = clf.fit(weather_data, weather_target)
+        _f1_score=f1_score(weather_target, clf.predict(weather_data), average="micro")
+        sheet1.cell(row=max_d+1, column=3).value=_f1_score
+        # print('max_depth='+str(max_d))
+        # print('f1_score:', _f1_score)
 
 def random_forest_f1(end_depth, end_estimator):
     # ランダムフォレストを作成してF値を計算 (木の数、木の深さはグリッドサーチ)
     for _depth in range(1,end_depth+1):
-        print('max_depth='+str(_depth))
-        sheet.cell(row=_depth+2, column=2).value=_depth
+        # print('max_depth='+str(_depth))
+        sheet2.cell(row=_depth+2, column=2).value=_depth
         for _estimator in range(1,end_estimator+1):
             rf = RandomForestClassifier(n_estimators=_estimator, max_depth=_depth, random_state=1)
             rf = rf.fit(weather_data, weather_target)
             _f1_score = f1_score(weather_target, rf.predict(weather_data), average="micro")
             
-            sheet.cell(row=2, column=_estimator+2).value=_estimator
-            sheet.cell(row=_depth+2, column=_estimator+2).value=_f1_score
-            print('n_estimators='+str(_estimator))
-            print('f1_score: '+str(_f1_score))
-        print()
+            sheet2.cell(row=2, column=_estimator+2).value=_estimator
+            sheet2.cell(row=_depth+2, column=_estimator+2).value=_f1_score
+        #     print('n_estimators='+str(_estimator))
+        #     print('f1_score: '+str(_f1_score))
+        # print()
 
-# decision_tree(max_d=5)
-# random_forest(end_depth=5, end_estimator=10)
-# feature_importances_calculator(n_est=8,max_d=4)
+print('決定木の深さと精度')
 
-# print('決定木の深さと精度')
-# for i in range(1,7):
-#     decision_tree_f1(max_d=i)
+decision_tree_f1(end_depth=10)
 
-# print()
+#print()
 
-print('ランダムフォレストの木の数・深さと精度')
-random_forest_f1(end_depth=7, end_estimator=10)
+#print('ランダムフォレストの木の数・深さと精度')
+random_forest_f1(end_depth=10, end_estimator=50)
 
 book.save('kadai10_table_rev.xlsx')
 book.close()
